030abd674c1a7bd6b2fe1a5238f6fd94273809d7
GenyaSochi <eunasedkina@gmail.com>
2026-02-08 21:33:42 +0300

    test

--- app/app.vue
+++ app/app.vue
@@ -2,6 +2,7 @@
   <nav style="display:flex; gap:10px">
     <NuxtLink to="/">home</NuxtLink>
     <NuxtLink to="/user">users</NuxtLink>
+    <NuxtLink to="/test">test</NuxtLink>
   </nav>
   <div>    
     <NuxtPage />
