01d799733e7f581c0327d70b0497931dc1f47c34
GenyaSochi <eunasedkina@gmail.com>
2026-02-19 21:39:45 +0300

    _

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,20 @@
 <template>
+  <nav class="navigation">
+    <NuxtLink to="/">main</NuxtLink>
+    <NuxtLink to="/posts">posts</NuxtLink>
+    <NuxtLink to="test">test</NuxtLink>
+    <NuxtLink to="user">user</NuxtLink>
+    <NuxtLink to="users">users</NuxtLink>
+  </nav>
   <div>    
     <NuxtPage />
   </div>
 </template>
+
+<style scoped>
+.navigation {
+display: flex;
+gap: 20px;
+background-color: rgb(155, 155, 218);
+}
+</style>
